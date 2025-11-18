daa236bbc44d29a11f423e6af97cd8e062811b39
Pablo Marinho <pablolucas-1@outlook.com>
2025-11-18 15:14:31 -0300

    criado um CRUD de posts com resources

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -10,56 +10,56 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return PostResource::collection(Post::all());
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return PostResource::collection(Post::with('author')->paginate(2));
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StorePostRequest $request)
-    {
-        $data = $request->validated();
-        $data['author_id'] = 1;
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StorePostRequest $request)
+  {
+    $data = $request->validated();
+    $data['author_id'] = 1;
 
-        $post = Post::create($data);
+    $post = Post::create($data);
 
-        return response()->json($post, 201);
-    }
+    return response()->json(new PostResource($post), 201);
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Post $post)
-    {
-        return response()->json($post);
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Post $post)
+  {
+    return response()->json(new PostResource($post));
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Post $post)
-    {
-        $data = $request->validate([
-            'title' => 'required|string|min:2',
-            'body' => ['required', 'string', 'min:2']
-        ]);
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Post $post)
+  {
+    $data = $request->validate([
+      'title' => 'required|string|min:2',
+      'body' => ['required', 'string', 'min:2']
+    ]);
 
-        $post->update($data);
+    $post->update($data);
 
-        return $post;
-    }
+    return new PostResource($post);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Post $post)
-    {
-        $post->delete();
-        return response()->noContent();
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Post $post)
+  {
+    $post->delete();
+    return response()->noContent();
+  }
 }
