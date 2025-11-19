6a5528031d3b9baa09861f4d57ce510011dbdb20
Pablo Marinho <pablolucas-1@outlook.com>
2025-11-19 05:48:55 -0300

    iniciado o CRUD do usuário

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/hello', function () {
-    return ['message' => 'Hello Laravel API'];
+  return ['message' => 'Hello Laravel API'];
 });
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('posts', PostController::class);
+  Route::apiResource('posts', PostController::class);
 });
+
+require __DIR__ . '/auth.php';
